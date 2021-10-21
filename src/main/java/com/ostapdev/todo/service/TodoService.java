b7069c9c5b9422a694f6fe3c62709bb32af90d2c
Danil.Ostapchuk <68132283+ostapkazak@users.noreply.github.com>
2021-10-21 20:39:03 +0300

    finish task-2

--- src/main/java/com/ostapdev/todo/service/TodoService.java
+++ src/main/java/com/ostapdev/todo/service/TodoService.java
@@ -3,7 +3,9 @@ package com.ostapdev.todo.service;
 import com.ostapdev.todo.dao.TodoDao;
 import com.ostapdev.todo.model.Task;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TodoService implements TodoDao {
@@ -11,7 +13,13 @@ public class TodoService implements TodoDao {
 
     @Override
     public void add(String taskDescription) {
-        tasks.put(1,new Task(taskDescription,false));
+        if (tasks.isEmpty()) {
+            tasks.put(1,new Task(taskDescription,false));
+            return;
+        }
+
+        List<Integer> keys = new ArrayList<>(tasks.keySet());
+        tasks.put(keys.get(keys.size()-1)+1,new Task(taskDescription,false));
     }
 
     @Override
@@ -35,8 +43,38 @@ public class TodoService implements TodoDao {
         tasks.forEach((id, task) -> printTask(isAll,task,id));
     }
 
+    @Override
+    public void delete(Integer taskId) {
+        Task task = tasks.get(taskId);
+        if (task == null){
+            System.out.println("Задачи с таким идентификатором нет");
+        }
+
+        else {
+            tasks.remove(taskId);
+        }
+    }
+
+    @Override
+    public void edit(Integer taskId, String taskDescription) {
+        Task task = tasks.get(taskId);
+        try {
+            task.setTaskDescription(taskDescription);
+            tasks.put(taskId,task);
+        }catch (NullPointerException e){
+            System.out.println("Задачи с таким идентификатором нет");
+        }
+    }
+
+    @Override
+    public void search(String substring) {
+        tasks.forEach((id,task)->{
+            if (task.getTaskDescription().contains(substring)) printTask(true,task,id);
+        });
+    }
+
     private void printTask(boolean isAllMark,Task task,Integer taskId){
-        if (isAllMark) System.out.println(taskId + ". " + (task.isDone() ? "X " : "") + task.getTaskDescription());
+        if (isAllMark) System.out.println(taskId + ". " + (task.isDone() ? "[X]" : "[ ]") + task.getTaskDescription());
 
         else {
             if (!task.isDone()) {
