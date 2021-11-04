d5ea26aeaa04ff01b9b33401d8af3ee557ed13e6
Danil.Ostapchuk <68132283+ostapkazak@users.noreply.github.com>
2021-11-04 14:04:01 +0300

    add logger to main class

--- src/main/java/com/ostapdev/todo/TodoApp.java
+++ src/main/java/com/ostapdev/todo/TodoApp.java
@@ -1,11 +1,13 @@
 package com.ostapdev.todo;
 
 import com.ostapdev.todo.parser.CommandParser;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+@Slf4j
 public class TodoApp {
     public static void main(String[] args) {
         CommandParser commandParser = new CommandParser();
@@ -14,6 +16,7 @@ public class TodoApp {
             try {
                 commandParser.parse(reader.readLine());
             }catch (IOException e){
+                log.error(e.toString());
                 System.out.println("IO");
             }
         }
