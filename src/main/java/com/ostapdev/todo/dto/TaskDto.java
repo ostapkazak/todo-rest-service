521cbc493e7e2505ddda0acd3c978a7ee37a3f4a
Danil.Ostapchuk <68132283+ostapkazak@users.noreply.github.com>
2021-11-29 16:14:28 +0300

    delete unnecessary anotations from taskDescription

--- src/main/java/com/ostapdev/todo/dto/TaskDto.java
+++ src/main/java/com/ostapdev/todo/dto/TaskDto.java
@@ -3,13 +3,9 @@ package com.ostapdev.todo.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class TaskDto {
-    @NotNull(message = "Task description may not be null")
     @NotBlank(message = "Task description may not be blank")
-    @NotEmpty(message = "Task description may not be empty")
     private String taskDescription;
 }
